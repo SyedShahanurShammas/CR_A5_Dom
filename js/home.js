26e5617a04c8e519750885214f6752e01bf657ae
Syed Shammas <sayedshaun2020@gmail.com>
2025-07-15 01:03:04 +0600

    functanality added

--- js/home.js
+++ js/home.js
@@ -1,8 +1,72 @@
-document.getElementById('btn-nowakhali-donate').addEventListener('click', (event) => {
-    console.log('clicked')
-    event.preventDefault();
-    const taka = getInputValueByid('input-nowakhali-donation');
-    console.log(taka)
 
+// Nowakhali section
+document.getElementById('btn-nowakhali-donate').addEventListener('click', (event) => {
+    event.preventDefault();
+    let addTaka = getInputValueById('input-nowakhali-donation');
+    //check
+    if (isNaN(addTaka)) {
+        alert('Please Enter Amount! You have written alphabet!')
+        return;
+    }
+    if (addTaka >= 0) {
+        let myAmount = getExistingValueFromText('my-current-amount');
+        myAmount -= addTaka;
+
+        let exisTingTaka = getExistingValueFromText('ExistingTaka-nowakhali')
+        exisTingTaka += addTaka;
+        // set the money into nowakhali
+        setTheMoney('ExistingTaka-nowakhali', exisTingTaka)
+        // set the money into my current amount
+        setTheMoney('my-current-amount', myAmount)
+    }
+    else {
+        alert('Negative number is not valid')
+    }
+})
+// feni section
+document.getElementById('btn-input-Donate-feni').addEventListener('click', (event) => {
+    event.preventDefault()
+    let addTaka = getInputValueById('input-value-fromFeni');
+    //check
+    if (isNaN(addTaka)) {
+        alert('Please Enter Amount! You have written alphabet!')
+        return;
+    }
+    if (addTaka >= 0) {
+        let myAmount = getExistingValueFromText('my-current-amount');
+        myAmount -= addTaka;
+
+        let exisTingTaka = getExistingValueFromText('existing-money-InFeni');
+        exisTingTaka += addTaka;
+
+        // set the money into feni
+        setTheMoney('existing-money-InFeni', exisTingTaka)
+        // set the money into my current amount
+        setTheMoney('my-current-amount', myAmount)
+    }
+    else {
+        alert('Negative number is not valid')
+    }
+})
+// Quota movement
+document.getElementById('btn-quota-movement').addEventListener('click', (event) => {
+    event.preventDefault();
+    let addTaka = getInputValueById('input-quota-movement');
+    //check
+    if (isNaN(addTaka)) {
+        alert('Please Enter Amount! You have written alphabet!')
+        return;
+    }
+    if (addTaka >= 0) {
+        let myAmount = getExistingValueFromText('my-current-amount');
+        myAmount -= addTaka;
+
+        let exisTingTaka = getExistingValueFromText('exisTing-amount-quotaMovement');
+        exisTingTaka += addTaka;
+        // set the money into feni
+        setTheMoney('exisTing-amount-quotaMovement', exisTingTaka)
+        // set the money into my current amount
+        setTheMoney('my-current-amount', myAmount)
+    }
 
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,5 +1,15 @@
-function getInputValueByid(id) {
+function getInputValueById(id) {
     const inputValue = document.getElementById(id).value;
     const NewInputValue = parseFloat(inputValue)
     return NewInputValue;
+}
+// funtion of geting value from existing value.....
+function getExistingValueFromText(text) {
+    const ExistingTextValue = document.getElementById(text).innerText;
+    const NewExsistingTextValue = parseFloat(ExistingTextValue);
+    return NewExsistingTextValue;
+}
+//set money
+function setTheMoney(id, money) {
+    document.getElementById(id).innerText = money;
 }
